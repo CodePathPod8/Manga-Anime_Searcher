32c4d3da78de12a7feaea97f6bd86f43fb6e4b9a
yunsanch <60577975+yunsanch@users.noreply.github.com>
2022-10-22 15:09:39 -0400

    initial stage

---
+++ Manga-Anime_searcher/Manga-Anime_searcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manga-Anime_searcher
+//
+//  Created by Yunior Sanchez on 10/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
